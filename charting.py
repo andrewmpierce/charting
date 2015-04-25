4c1d5f9d61006b46e6031ca4d7b367410a817e04
apierce1992 <apierce1992@gmail.com>
2015-04-24 20:40:37 -0400

    added histogram

--- charting.py
+++ charting.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 def flip_coin(n):
 	coin_outcomes = [0,1]
 	record_outcome = []
-	
 	for i in range(n):
 		record_outcome.append(random.choice(coin_outcomes))		
 	return record_outcome
 
+
 def flip_distribution(x, n, flip_coin):
         results = []
         for i in range(x):
-                avg = (sum(flip_coin(n))/ float(n))*100
+                avg = (sum(flip_coin(n))/ float(n))
                 results.append(avg)
         return results
 
@@ -26,5 +26,9 @@ def x_axis(x):
                 
         return x_values
 
-plt.scatter(x_axis(500), flip_distribution(500, 1000, flip_coin))
+plt.axis([0, 500, 0, 1])
+plt.plot(x_axis(500), flip_distribution(500, 10000, flip_coin))
+plt.show()
+
+plt.hist(flip_distribution(500, 10000, flip_coin), bins=3)
 plt.show()
